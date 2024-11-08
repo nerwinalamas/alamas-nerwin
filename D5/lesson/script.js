7503844566f3d99baade19f8e8df134dab5827af
nerwinalamas <nerwinalamas@gmail.com>
2024-11-08 21:42:38 +0800

    Day 5 Lesson: variables, data types, operators and expressions, assignment operator, logical operators, and loops

---
+++ D5/lesson/script.js
@@ -0,0 +1,101 @@
+// Variables
+
+// let name = 'John';
+// name = 'Paul';
+// const temp = 100;
+
+// Outputting variables
+// console.log(name);
+// console.log(temp);
+
+// Basic Syntax
+// let x = 5;
+// let y = 10;
+// let sum = x + y;
+// console.log(sum);
+
+// Primitive types: These are the most basic data types in JavaScript:
+// Number: Represents numeric values. For example, 5, 3.14, -10.
+// String: Represents textual data enclosed in single ('') or double ("") quotes. For example, "Hello", 'JavaScript'.
+// Boolean: Represents either true or false (logical values).
+// Null: Represents the intentional absence of any object value.
+// Undefined: Represents an uninitialized or missing value.
+
+// Primitive data types
+let name = "John"; // string
+let age = 25; // number
+let isStudent = true; // boolean
+let car = null; // null
+let city; // undefined
+
+// Outputting Data Types
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of isStudent:", typeof isStudent);
+console.log("Type of car:", typeof car);
+console.log("Type of city:", typeof city);
+
+// Operators and Expressions
+//
+
+let x = 5;
+let y = 2;
+
+let sum = x + y;
+console.log("Sum: ", sum);
+
+let difference = x - y;
+console.log("Difference: ", difference);
+
+let product = x * y;
+console.log("Product: ", product);
+
+let quotient = x / y;
+console.log("Quotient: ", quotient);
+
+let remainder = x % y;
+console.log("Remainder: ", remainder);
+
+// Assignment operator
+let a = 10;
+let b = 5;
+
+a += b;
+console.log("a: ", a);
+
+a -= b;
+console.log("a: ", a);
+
+a *= b;
+console.log("a:", a);
+
+a /= b;
+console.log("a:", a);
+
+// Comparison Operators
+let p = 3;
+let q = 6;
+
+console.log("p > q:", p > q);
+console.log("p < q:", p < q);
+console.log("p >= q:", p >= q);
+console.log("p <= q:", p <= q);
+console.log("p === q:", p === q);
+console.log("p == q:", p == q);
+
+// Logical Operators
+let sunny = true;
+let warm = false;
+
+console.log("True and False: ", sunny && warm);
+console.log("True and True: ", sunny && sunny);
+console.log("True or False: ", sunny || warm);
+console.log("Not True: ", !sunny);
+
+
+
+
+
+
+
+
---
+++ D5/lesson/script2.js
@@ -0,0 +1,39 @@
+let age = 18;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+// Loops
+
+// while loop
+let count = 1;
+
+while (count <= 5) {
+    console.log("Count: ", count);
+    count++;
+}
+
+// do while loop
+let i = 1;
+
+do {
+    console.log("Do while count: ", i);
+    i++;
+} while (i <= 5);
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Count: ", i);
+}
+
+// alert("Hello");
+// let name = prompt("Enter Name");
+// alert("Hello " + name)
+
+let number = parseInt(prompt("Enter Number"))
+alert(20 + number)
+
+
