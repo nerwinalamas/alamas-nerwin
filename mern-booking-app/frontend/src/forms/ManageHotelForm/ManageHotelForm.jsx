78bc40ed6ebc741d5d96ca5a9d1ba99992d87058
nerwinalamas <nerwinalamas@gmail.com>
2024-12-06 21:46:18 +0800

    Day 23 Asynchronous Lesson

--- mern-booking-app/frontend/src/forms/ManageHotelForm/ManageHotelForm.jsx
+++ mern-booking-app/frontend/src/forms/ManageHotelForm/ManageHotelForm.jsx
@@ -1,16 +1,66 @@
-import React from 'react'
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
+import TypeSection from "./TypeSection";
+import FacilitiesSection from "./FacilitiesSection";
+import GuestsSection from "./GuestsSection";
+import ImagesSection from "./ImagesSection";
 
-const ManageHotelForm = () => {
+const ManageHotelForm = ({ onSave, isLoading }) => {
     const formMethods = useForm();
-  return (
-    <FormProvider {...formMethods}>
-        <form>
-            <DetailsSection />
-        </form>
-    </FormProvider>
-  )
-}
+    const { handleSubmit, reset } = formMethods;
 
-export default ManageHotelForm
+    const onSubmit = handleSubmit((formDataJson) => {
+        const formData = new FormData();
+        // if (hotel) {
+        //     formData.append("hotelId", hotel._id);
+        // }
+        formData.append("name", formDataJson.name);
+        formData.append("city", formDataJson.city);
+        formData.append("country", formDataJson.country);
+        formData.append("description", formDataJson.description);
+        formData.append("type", formDataJson.type);
+        formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+        formData.append("starRating", formDataJson.starRating.toString());
+        formData.append("adultCount", formDataJson.adultCount.toString());
+        formData.append("childCount", formDataJson.childCount.toString());
+
+        formDataJson.facilities.forEach((facility, index) => {
+            formData.append(`facilities[${index}]`, facility);
+        });
+
+        if (formDataJson.imageUrls) {
+            formDataJson.imageUrls.forEach((url, index) => {
+                formData.append(`imageUrls[${index}]`, url);
+            });
+        }
+
+        Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+            formData.append(`imageFiles`, imageFile);
+        });
+
+        onSave(formData);
+    });
+
+    return (
+        <FormProvider {...formMethods}>
+            <form className="flex flex-col gap-10" onSubmit={onSubmit}>
+                <DetailsSection />
+                <TypeSection />
+                <FacilitiesSection />
+                <GuestsSection />
+                <ImagesSection />
+                <span className="flex justify-end">
+                    <button
+                        disabled={isLoading}
+                        type="submit"
+                        className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled:bg-gray-500"
+                    >
+                        {isLoading ? "Saving..." : "Save"}
+                    </button>
+                </span>
+            </form>
+        </FormProvider>
+    );
+};
+
+export default ManageHotelForm;
--- mern-booking-app/frontend/src/pages/AddHotel.jsx
+++ mern-booking-app/frontend/src/pages/AddHotel.jsx
@@ -1,13 +1,25 @@
-import React from "react"; 
+import { useMutation } from "react-query";
+import { useAppContext } from "../contexts/AppContext.jsx";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm.jsx"; // Form component for managing hotel data.
-
+import * as apiClient from "../api-client";
 
 const AddHotel = () => {
+    const { showToast } = useAppContext();
 
-  return (
+    const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+        onSuccess: () => {
+            showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+        },
+        onError: () => {
+            showToast({ message: "Error Saving Hotel", type: "ERROR" });
+        },
+    });
 
-    <ManageHotelForm />
-  );
+    const handleSave = (hotelFormData) => {
+        mutate(hotelFormData);
+    };
+
+    return <ManageHotelForm onSave={handleSave} isLoading={isLoading} />;
 };
 
 export default AddHotel;
